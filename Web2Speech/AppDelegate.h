724e91aabd1bb9120409a15dcd713123bb9241e6
James Baker <james@frigidsoftware.com>
2014-07-04 17:35:44 -0400

    More abstraction.

---
+++ Web2Speech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Web2Speech
+//
+//  Created by James on 7/4/14.
+//  Copyright (c) 2014 Frigid Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Web2Speech/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Web2Speech
+//
+//  Created by James on 7/4/14.
+//  Copyright (c) 2014 Frigid Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import AVFoundation;
+
+@interface ViewController : UIViewController <AVSpeechSynthesizerDelegate>
+
+
+@end
+
