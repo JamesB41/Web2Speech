cd252e575db8d9952a45b80cebe8760196f04f42
James Baker <james@frigidsoftware.com>
2014-07-04 14:18:15 -0400

    Initial Commit

---
+++ Bridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bridge
+//
+//  Created by James on 7/4/14.
+//  Copyright (c) 2014 Frigid Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
